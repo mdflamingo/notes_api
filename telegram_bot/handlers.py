58f9ccbf62445262a92da978a2fe6dcdaa6a7091
Анастасия Искоскина <aiskoskina@yandex.ru>
2024-09-18 16:22:46 +0300

    - create bot infrastructure

---
+++ telegram_bot/handlers.py
@@ -0,0 +1,20 @@
+from aiogram import types, F, Router
+from aiogram.types import Message
+from aiogram.filters import Command
+
+
+router = Router()
+
+
+@router.message(Command("start"))
+async def start_handler(msg: Message):
+    await msg.answer("Привет! Вот что я могу: "
+                     "4. Создать заметку "
+                     "3. Показать все твои заметки "
+                     "2. Найти заметки по тегам "
+                     "1. Зарегистрировать в приложении 'Note service' ")
+
+
+@router.message()
+async def message_handler(msg: Message):
+    await msg.answer(f"Твой ID: {msg.from_user.id}")
